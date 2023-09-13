f4d93a8e6f529584f314483f0c871592b7fa775f
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-09-13 13:16:24 -0400

    Create vc-mapping.py

---
+++ vc-mapping.py
@@ -0,0 +1,17 @@
+import openhmd
+
+# Placeholder function to initialize OpenHMD
+def init_openhmd():
+    # (Initialize OpenHMD here)
+    pass
+
+# Placeholder function to update inputs each frame
+def update():
+    # (Get data from OpenHMD and set mouse position here)
+    pass
+
+# Initialize OpenHMD
+init_openhmd()
+
+# Set the update function to be called each frame
+freepie.run(update)
