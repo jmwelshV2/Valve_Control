791a96d42f53210a204fb73b3360a472fc3c92f2
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-09-13 13:22:36 -0400

    Create vc-start.sh

---
+++ vc-start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Starting the Valve Control testing and initial setup script..."
+sleep 2
+
+echo "Step 1: Testing individual components."
+sleep 2
+
+echo "Testing XRDesktop..."
+# (Run a command to test XRDesktop here)
+sleep 2
+
+echo "Testing OpenHMD..."
+# (Run a command to test OpenHMD here)
+sleep 2
+
+echo "Testing FreePIE..."
+# (Run a command to test FreePIE here)
+sleep 2
+
+echo "Testing Monado..."
+# (Run a command to test Monado here)
+sleep 2
+
+echo "Individual components testing completed. Please check above for any errors."
+sleep 2
+
+echo "Step 2: XRDesktop configuration."
+echo "Please configure XRDesktop according to the on-screen instructions."
+# (Run a command to open XRDesktop configuration here)
+sleep 2
+
+echo "Step 3: FreePIE input mapping."
+echo "Next, we will proceed with FreePIE input mapping."
+echo "Please follow the instructions to set up basic input mapping in FreePIE."
+# (Run a command to open FreePIE script editor here)
+sleep 2
+
+echo "Testing and initial setup completed. Please review any errors and configure the settings as needed."
+echo "Refer to the documentation for further guidance."
